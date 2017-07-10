332eb602599e2aa3bfd624c1c790603d0dca5500
OOPer <nagata@shlab.jp>
2017-07-10 10:29:34 +0900

    Initial Commit

---
+++ VisionFace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionFace
+//
+//  Created by 開発 on 2017/7/10.
+//  Copyright © 2017 OOPer (NAGATA, Atsuyuki). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
